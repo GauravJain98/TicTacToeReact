c5838213287c51e23701acdf3e96fcf2f7ec9888
Gaurav Jain <crazcuber@gmail.com>
2020-11-30 15:03:45 +0530

    disable repeat click

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,28 +5,62 @@ export default class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      winnner: null,
+      error: null,
+      winner: null,
       status: "Next player: X",
       square: Array(9).fill(null),
       isPlayerX: true, // true is X false is O
     };
   }
-  handleSquareClick(i) {
-    const square = this.state.square.slice();
-    square[i] = this.state.isPlayerX ? "X" : "O";
-    let status;
-    if (this.state.winner === undefined) {
-      status = this.state.isPlayerX ? "Next player: O" : "Next player: X";
-    } else {
-      status = "Winner is " + square[i];
+  winner(pt) {
+    let i;
+    //Diagonal 1
+    if (pt / 2 === 0 && pt / 2 < 4 && 0 < pt) {
+      for (i = 1; i < 4; i++) {
+        if (this.state.square[i * 2] !== this.state.square[pt]) {
+          return false;
+        }
+      }
+      return true;
     }
-    console.log(this.state.winner);
-    console.log(this.state.winner === undefined);
-    this.setState({
-      square: square,
-      isPlayerX: !this.state.isPlayerX,
-      status: status,
-    });
+    //Diagonal 2
+    if (pt / 4 === 0) {
+      for (i = 0; i < 3; i++) {
+        if (this.state.square[i * 4] !== this.state.square[pt]) {
+          return false;
+        }
+      }
+    }
+    //Horizontal
+  }
+  handleSquareClick(i) {
+    if (this.state.square[i] === null) {
+      const square = this.state.square.slice();
+      square[i] = this.state.isPlayerX ? "X" : "O";
+      let status;
+      this.setState({
+        square: square,
+      });
+      // this.setState({
+      //   winner: this.winner(i),
+      // });
+      if (this.state.winner === null) {
+        status = this.state.isPlayerX ? "Next player: O" : "Next player: X";
+      } else {
+        status = "Winner is " + square[i];
+      }
+      // console.log(this.state.winner);
+      // console.log(this.state.winner === null);
+      this.setState({
+        isPlayerX: !this.state.isPlayerX,
+        status: status,
+        error: null,
+      });
+    } else {
+      this.setState({
+        error: "Choose another Square",
+      });
+    }
   }
   renderSquare(i) {
     return (
@@ -41,6 +75,7 @@ export default class Board extends React.Component {
     return (
       <div>
         <div className="status">{this.state.status}</div>
+        <div className="status">{this.state.error}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
